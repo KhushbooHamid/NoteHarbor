a7db4e1cc005c262b5d2043083e5d3b278849b91
Khushi <92801019+KhushbooHamid@users.noreply.github.com>
2024-01-25 13:08:49 +0530

    make note editable

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -4,7 +4,7 @@ import noteContext from "../context/notes/noteContext";
 
 const NoteItem = (props) => {
   const context = useContext(noteContext);
-  const {deleteNote} = context;
+  const {deleteNote,  updateNote} = context;
 
   const { note } = props;
   return (
@@ -14,7 +14,8 @@ const NoteItem = (props) => {
           <div className="d-flex align-items-center">
             <h5 className="card-title">{note.title}</h5>
             <i className="fa-solid fa-trash mx-2" onClick={()=>{deleteNote(note._id)}}></i>
-            <i className="fa-solid fa-pen-to-square mx-2"></i>
+            <i className="fa-solid fa-pen-to-square mx-2 "  onClick={()=>{props.updateNote(note)}}  ></i>
+            {/* data-bs-toggle="modal" data-bs-target="#exampleModal" */}
           </div>
           <p className="card-text">{note.description}</p>
         </div>
